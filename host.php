c37e187125028975ca58db87ef4a64aa2f8b5795
disstudio <disstudio1990@gmail.com>
2022-04-19 12:18:53 +0300

    Update host.php

--- host.php
+++ host.php
@@ -1,49 +1,37 @@
 {
     "sites": [
         {
-            "page": "62.76.145.81",
-            "points": [
-                {
-                    "port": 80,
-                    "protocol": "tcp"
-                },
-                {
-                    "port": 443,
-                    "protocol": "tcp"
-                }
-            ],
-            "url": "mercury.vetrf.ru"
+            "page": "www.aviasales.ru"
         },
         {
-            "page": "62.76.145.80",
-            "points": [
-                {
-                    "port": 80,
-                    "protocol": "tcp"
-                },
-                {
-                    "port": 443,
-                    "protocol": "tcp"
-                }
-            ],
-            "url": "api.vetrf.ru"
+            "page": "www.aviasales.ru"
         },
         {
-            "page": "62.76.145.85",
-            "points": [
-                {
-                    "port": 80,
-                    "protocol": "tcp"
-                },
-                {
-                    "port": 443,
-                    "protocol": "tcp"
-                }
-            ],
-            "url": "api2.vetrf.ru"
+            "page": "aviasales.ru"
         },
         {
-            "page": "62.76.145.83",
+            "page": "b2b.aviasales.ru"
+        },
+        {
+            "page": "calendar.aviasales.ru"
+        },
+        {
+            "page": "tickets.aviasales.ru"
+        },
+        {
+            "page": "search.aviasales.ru"
+        },
+        {
+            "page": "7.aviasales.ru"
+        },
+        {
+            "page": "wwb1a.beta.aviasales.ru"
+        },
+        {
+            "page": "freeipa.beta.aviasales.ru"
+        },
+        {
+            "page": "193.24.8.15",
             "points": [
                 {
                     "port": 80,
@@ -54,70 +42,318 @@
                     "protocol": "tcp"
                 }
             ],
-            "url": "idp.vetrf.ru\/idp\/profile\/SAML2\/POST\/SSO?execution=e1s1"
+            "url": "nordwindairlines.ru\/ru"
+        },
+        {
+            "page": "85.31.123.212",
+            "points": [
+                {
+                    "port": 80,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 443,
+                    "protocol": "tcp"
+                }
+            ],
+            "url": "cas.nordwindairlines.ru\/nwa\/index.nwa?ResX=1180?ResY=932"
+        },
+        {
+            "page": "85.31.124.50",
+            "points": [
+                {
+                    "port": 80,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 443,
+                    "protocol": "tcp"
+                }
+            ],
+            "url": "appletest.nordwindairlines.ru"
+        },
+        {
+            "page": "85.31.126.157",
+            "points": [
+                {
+                    "port": 80,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 443,
+                    "protocol": "tcp"
+                }
+            ],
+            "url": "lydia.nordwindairlines.ru\/SKYPSS\/index.jsp"
+        },
+        {
+            "page": "85.31.124.232",
+            "points": [
+                {
+                    "port": 80,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 443,
+                    "protocol": "tcp"
+                }
+            ],
+            "url": "legal.nordwindairlines.ru\/login"
+        },
+        {
+            "page": "37.220.162.162",
+            "points": [
+                {
+                    "port": 80,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 443,
+                    "protocol": "tcp"
+                }
+            ],
+            "url": "www.aeroflot.ru\/ru-ru"
+        },
+        {
+            "page": "80.92.36.160",
+            "points": [
+                {
+                    "port": 80,
+                    "protocol": "tcp"
+                }
+            ],
+            "url": "ppls-vc.aeroflot.ru\/system\/login?domain=ppls-vc.f5.aeroflot.ru&next"
+        },
+        {
+            "page": "78.155.201.8",
+            "points": [
+                {
+                    "port": 22,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 443,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 8090,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 8090,
+                    "protocol": "http"
+                }
+            ],
+            "url": "track.aeroflot.ru"
+        },
+        {
+            "page": "80.92.36.6",
+            "points": [
+                {
+                    "port": 80,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 443,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 8060,
+                    "protocol": "tcp"
+                }
+            ],
+            "url": "click.aeroflot.ru"
+        },
+        {
+            "page": "80.92.37.14",
+            "points": [
+                {
+                    "port": 443,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 5060,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 5061,
+                    "protocol": "tcp"
+                }
+            ],
+            "url": "join.cms.aeroflot.ru\/ru-RU\/home"
+        },
+        {
+            "page": "80.92.36.74",
+            "points": [
+                {
+                    "port": 80,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 443,
+                    "protocol": "tcp"
+                }
+            ],
+            "url": "mobprd.aeroflot.ru"
+        },
+        {
+            "page": "178.159.249.161",
+            "points": [
+                {
+                    "port": 25,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 80,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 443,
+                    "protocol": "tcp"
+                }
+            ],
+            "url": "azimuth.aero\/ru"
+        },
+        {
+            "page": "109.238.243.245",
+            "points": [
+                {
+                    "port": 80,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 443,
+                    "protocol": "tcp"
+                }
+            ],
+            "url": "agents.azimuth.aero"
+        },
+        {
+            "page": "109.238.243.253",
+            "points": [
+                {
+                    "port": 80,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 443,
+                    "protocol": "tcp"
+                }
+            ],
+            "url": "agents.azimuth.aero"
+        },
+        {
+            "page": "92.53.126.171",
+            "points": [
+                {
+                    "port": 21,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 80,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 443,
+                    "protocol": "tcp"
+                }
+            ],
+            "url": "kiyaviakrym.com"
+        },
+        {
+            "page": "213.189.217.114",
+            "points": [
+                {
+                    "port": 22,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 80,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 443,
+                    "protocol": "tcp"
+                }
+            ],
+            "url": "rov.aero"
         }
     ],
     "proxy": [
-        {
-            "id": 1,
-            "ip": "23.108.47.124:80",
-            "auth": "ocelot:oempg4uvlf"
-        }, {
-            "id": 12,
-            "ip": "45.137.60.169:6697",
-            "auth": "perytmmv:72b7f5qclg3n"
-        }, {
-            "id": 32,
-            "ip": "154.95.36.87:6781",
-            "auth": "perytmmv:72b7f5qclg3n"
-        }, {
-            "id": 49,
-            "ip": "45.136.231.151:7207",
-            "auth": "perytmmv:72b7f5qclg3n"
-        }, {
-            "id": 94,
-            "ip": "91.246.194.120:6633",
-            "auth": "perytmmv:72b7f5qclg3n"
-        }, {
-            "id": 95,
-            "ip": "91.246.195.155:6924",
-            "auth": "perytmmv:72b7f5qclg3n"
-        }, {
-            "id": 123,
-            "ip": "95.164.232.101:5192",
-            "auth": "perytmmv:72b7f5qclg3n"
-        }, {
-            "id": 134,
-            "ip": "45.137.43.52:7606",
-            "auth": "perytmmv:72b7f5qclg3n"
-        }, {
-            "id": 135,
-            "ip": "185.213.242.135:8599",
-            "auth": "perytmmv:72b7f5qclg3n"
-        }, {
-            "id": 148,
-            "ip": "45.137.43.66:7620",
-            "auth": "perytmmv:72b7f5qclg3n"
-        }, {
-            "id": 161,
-            "ip": "154.92.122.179:5249",
-            "auth": "perytmmv:72b7f5qclg3n"
-        }, {
-            "id": 224,
-            "ip": "45.154.228.110:8134",
-            "auth": "perytmmv:72b7f5qclg3n"
-        }, {
-            "id": 243,
-            "ip": "45.130.60.135:9662",
-            "auth": "perytmmv:72b7f5qclg3n"
-        }, {
-            "id": 256,
-            "ip": "45.154.228.231:8255",
-            "auth": "perytmmv:72b7f5qclg3n"
-        }, {
-            "id": 262,
-            "ip": "209.127.191.180:9279",
-            "auth": "guaohxlt:fx95biv9hnvi"
-        }
-    ]
+{
+"id": 37,
+"ip": "164.70.121.242:3128",
+"auth": ""
+},
+{
+"id": 94,
+"ip": "103.248.25.99:80",
+"auth": ""
+},
+{
+"id": 368,
+"ip": "185.213.242.44:8508",
+"auth": "perytmmv:72b7f5qclg3n"
+},
+{
+"id": 378,
+"ip": "45.154.228.76:8100",
+"auth": "perytmmv:72b7f5qclg3n"
+},
+{
+"id": 710,
+"ip": "45.15.129.95:5056",
+"auth": "spiznxfg:r6daod3mfgkz"
+},
+{
+"id": 797,
+"ip": "95.164.233.148:5506",
+"auth": "spiznxfg:r6daod3mfgkz"
+},
+{
+"id": 799,
+"ip": "95.164.232.7:5098",
+"auth": "spiznxfg:r6daod3mfgkz"
+},
+{
+"id": 840,
+"ip": "95.164.235.205:6261",
+"auth": "spiznxfg:r6daod3mfgkz"
+},
+{
+"id": 1009,
+"ip": "95.164.234.236:5941",
+"auth": "spiznxfg:r6daod3mfgkz"
+},
+{
+"id": 1011,
+"ip": "23.236.216.161:6191",
+"auth": "spiznxfg:r6daod3mfgkz"
+},
+{
+"id": 1181,
+"ip": "95.164.235.175:6231",
+"auth": "spiznxfg:r6daod3mfgkz"
+},
+{
+"id": 1318,
+"ip": "95.164.235.207:6263",
+"auth": "spiznxfg:r6daod3mfgkz"
+},
+{
+"id": 1337,
+"ip": "95.164.233.159:5517",
+"auth": "spiznxfg:r6daod3mfgkz"
+},
+{
+"id": 1349,
+"ip": "95.164.232.155:5246",
+"auth": "spiznxfg:r6daod3mfgkz"
+},
+{
+"id": 1478,
+"ip": "95.164.233.81:5439",
+"auth": "spiznxfg:r6daod3mfgkz"
+}
+]
 }
